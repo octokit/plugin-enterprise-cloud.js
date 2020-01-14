051d70f2eb16cf0e414812baa18288e5493de804
Gregor Martynus <gregor@martynus.net>
2020-01-13 16:17:29 -0800

    build: remove params, add renamed

--- scripts/update-endpoints/code.js
+++ scripts/update-endpoints/code.js
@@ -5,76 +5,43 @@ const ENDPOINTS = require("./generated/endpoints.json");
 const newRoutes = {};
 
 ENDPOINTS.forEach(endpoint => {
-  if (endpoint.scope !== "scim") {
-    return;
+  if (!newRoutes[endpoint.scope]) {
+    newRoutes[endpoint.scope] = {};
   }
 
   const idName = endpoint.id;
 
   // new route
-  newRoutes[idName] = {
+  newRoutes[endpoint.scope][idName] = {
     method: endpoint.method,
+    url: endpoint.url.replace(/\{([^}]+)}/g, ":$1"),
     headers: endpoint.headers.reduce((result, header) => {
       if (!result) {
         result = {};
       }
       result[header.name] = header.value;
       return result;
-    }, undefined),
-    params: endpoint.parameters.reduce((result, param) => {
-      result[param.name] = {
-        type: param.type
-      };
-      if (param.allowNull) {
-        result[param.name].allowNull = true;
-      }
-      if (param.required) {
-        result[param.name].required = true;
-      }
-      if (param.mapToData) {
-        result[param.name].mapTo = "data";
-      }
-      if (param.enum) {
-        result[param.name].enum = param.enum;
-      }
-      if (param.validation) {
-        result[param.name].validation = param.validation;
-      }
-      if (param.deprecated) {
-        result[param.name].deprecated = true;
-
-        if (param.alias) {
-          result[param.name].alias = param.alias;
-          result[param.name].type = result[param.alias].type;
-        } else {
-          result[param.name].type = param.type;
-          result[param.name].description = param.description;
-        }
-      }
-
-      return result;
-    }, {}),
-    url: endpoint.url.replace(/\{([^}]+)}/g, ":$1")
+    }, undefined)
   };
 
-  const previewHeaders = endpoint.previews
-    .map(preview => `application/vnd.github.${preview.name}-preview+json`)
-    .join(",");
-
-  if (previewHeaders) {
-    newRoutes[idName].headers = {
-      accept: previewHeaders
-    };
+  if (endpoint.previews.length) {
+    if (!newRoutes[endpoint.scope][idName].mediaType) {
+      newRoutes[endpoint.scope][idName].mediaType = {};
+    }
+    newRoutes[endpoint.scope][
+      idName
+    ].mediaType.previews = endpoint.previews.map(preview => preview.name);
   }
 
   if (endpoint.renamed) {
-    newRoutes[
-      idName
-    ].deprecated = `octokit.${endpoint.renamed.before.scope}.${endpoint.renamed.before.id}() has been renamed to octokit.${endpoint.renamed.after.scope}.${endpoint.renamed.after.id}() (${endpoint.renamed.date})`;
+    newRoutes[endpoint.scope][idName].renamed = [
+      [endpoint.renamed.before.scope, endpoint.renamed.before.id],
+      [endpoint.renamed.after.scope, endpoint.renamed.after.id]
+    ];
   }
 
   if (endpoint.isDeprecated) {
-    newRoutes[
+    newRoutes[endpoint.scope][
       idName
     ].deprecated = `octokit.scim.${idName}() is deprecated, see ${endpoint.documentationUrl}`;
   }
