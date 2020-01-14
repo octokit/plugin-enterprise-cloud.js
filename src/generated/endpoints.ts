62068049f9e048f0e83fb2c80fe65cb9ef1b2273
Gregor Martynus <gregor@martynus.net>
2020-01-13 16:28:03 -0800

    build: replace routes.json with src/generated/endpoints.ts

---
+++ src/generated/endpoints.ts
@@ -0,0 +1,66 @@
+export default {
+  orgs: {
+    listCredentialAuthorizations: [
+      { method: "GET", url: "/orgs/:org/credential-authorizations" }
+    ],
+    removeCredentialAuthorization: [
+      {
+        method: "DELETE",
+        url: "/orgs/:org/credential-authorizations/:credential_id"
+      }
+    ]
+  },
+  scim: {
+    getProvisioningDetailsForUser: [
+      { method: "GET", url: "/scim/v2/organizations/:org/Users/:scim_user_id" }
+    ],
+    listProvisionedIdentities: [
+      { method: "GET", url: "/scim/v2/organizations/:org/Users" }
+    ],
+    provisionAndInviteUsers: [
+      { method: "POST", url: "/scim/v2/organizations/:org/Users" }
+    ],
+    provisionInviteUsers: [
+      { method: "POST", url: "/scim/v2/organizations/:org/Users" },
+      {
+        renamed: [
+          ["scim", "provisionInviteUsers"],
+          ["scim", "provisionAndInviteUsers"]
+        ]
+      }
+    ],
+    removeUserFromOrg: [
+      {
+        method: "DELETE",
+        url: "/scim/v2/organizations/:org/Users/:scim_user_id"
+      }
+    ],
+    replaceProvisionedUserInformation: [
+      { method: "PUT", url: "/scim/v2/organizations/:org/Users/:scim_user_id" }
+    ],
+    updateProvisionedOrgMembership: [
+      { method: "PUT", url: "/scim/v2/organizations/:org/Users/:scim_user_id" },
+      {
+        renamed: [
+          ["scim", "updateProvisionedOrgMembership"],
+          ["scim", "replaceProvisionedUserInformation"]
+        ]
+      }
+    ],
+    updateUserAttribute: [
+      {
+        method: "PATCH",
+        url: "/scim/v2/organizations/:org/Users/:scim_user_id"
+      }
+    ]
+  },
+  teams: {
+    createOrUpdateIdPGroupConnections: [
+      { method: "PATCH", url: "/teams/:team_id/team-sync/group-mappings" }
+    ],
+    listIdPGroups: [
+      { method: "GET", url: "/teams/:team_id/team-sync/group-mappings" }
+    ],
+    listIdPGroupsForOrg: [{ method: "GET", url: "/orgs/:org/team-sync/groups" }]
+  }
+};
