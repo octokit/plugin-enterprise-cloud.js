a278297e380a973ae72137fc89460f1a11f005d6
Keegan Campbell <me@kfcampbell.com>
2023-06-08 15:48:01 -0700

    Undo endpoints changes

--- src/generated/endpoints.ts
+++ src/generated/endpoints.ts
@@ -13,11 +13,8 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
     deleteSelfHostedRunnerGroupFromOrg: [
       "DELETE /orgs/:org/actions/runner-groups/:runner_group_id",
     ],
-    getActionsCacheUsageForEnterprise: [
-      "GET /enterprises/:enterprise/actions/cache/usage",
-    ],
-    getGithubActionsDefaultWorkflowPermissionsEnterprise: [
-      "GET /enterprises/:enterprise/actions/permissions/workflow",
+    getCustomOidcSubClaimForRepo: [
+      "GET /repos/:owner/:repo/actions/oidc/customization/sub",
     ],
     getSelfHostedRunnerGroupForOrg: [
       "GET /orgs/:org/actions/runner-groups/:runner_group_id",
@@ -38,8 +35,8 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
     setActionsOidcCustomIssuerPolicyForEnterprise: [
       "PUT /enterprises/:enterprise/actions/oidc/customization/issuer",
     ],
-    setGithubActionsDefaultWorkflowPermissionsEnterprise: [
-      "PUT /enterprises/:enterprise/actions/permissions/workflow",
+    setCustomOidcSubClaimForRepo: [
+      "PUT /repos/:owner/:repo/actions/oidc/customization/sub",
     ],
     setRepoAccessToSelfHostedRunnerGroupInOrg: [
       "PUT /orgs/:org/actions/runner-groups/:runner_group_id/repositories",
@@ -69,12 +66,6 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
     getGithubActionsBillingGhe: [
       "GET /enterprises/:enterprise/settings/billing/actions",
     ],
-    getGithubAdvancedSecurityBillingGhe: [
-      "GET /enterprises/:enterprise/settings/billing/advanced-security",
-    ],
-    getGithubAdvancedSecurityBillingOrg: [
-      "GET /orgs/:org/settings/billing/advanced-security",
-    ],
     getGithubPackagesBillingGhe: [
       "GET /enterprises/:enterprise/settings/billing/packages",
     ],
@@ -82,15 +73,7 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
       "GET /enterprises/:enterprise/settings/billing/shared-storage",
     ],
   },
-  codeScanning: {
-    listAlertsForEnterprise: [
-      "GET /enterprises/:enterprise/code-scanning/alerts",
-    ],
-  },
   enterpriseAdmin: {
-    addCustomLabelsToSelfHostedRunnerForEnterprise: [
-      "POST /enterprises/:enterprise/actions/runners/:runner_id/labels",
-    ],
     addOrgAccessToSelfHostedRunnerGroupInEnterprise: [
       "PUT /enterprises/:enterprise/actions/runner-groups/:runner_group_id/organizations/:org_id",
     ],
@@ -112,20 +95,8 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
     deleteSelfHostedRunnerGroupFromEnterprise: [
       "DELETE /enterprises/:enterprise/actions/runner-groups/:runner_group_id",
     ],
-    disableSelectedOrganizationGithubActionsEnterprise: [
-      "DELETE /enterprises/:enterprise/actions/permissions/organizations/:org_id",
-    ],
-    enableSelectedOrganizationGithubActionsEnterprise: [
-      "PUT /enterprises/:enterprise/actions/permissions/organizations/:org_id",
-    ],
-    getAllowedActionsEnterprise: [
-      "GET /enterprises/:enterprise/actions/permissions/selected-actions",
-    ],
     getAuditLog: ["GET /enterprises/:enterprise/audit-log"],
     getConsumedLicenses: ["GET /enterprises/:enterprise/consumed-licenses"],
-    getGithubActionsPermissionsEnterprise: [
-      "GET /enterprises/:enterprise/actions/permissions",
-    ],
     getLicenseSyncStatus: ["GET /enterprises/:enterprise/license-sync-status"],
     getSelfHostedRunnerForEnterprise: [
       "GET /enterprises/:enterprise/actions/runners/:runner_id",
@@ -133,20 +104,11 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
     getSelfHostedRunnerGroupForEnterprise: [
       "GET /enterprises/:enterprise/actions/runner-groups/:runner_group_id",
     ],
-    getServerStatistics: [
-      "GET /enterprise-installation/:enterprise_or_org/server-statistics",
-    ],
-    listLabelsForSelfHostedRunnerForEnterprise: [
-      "GET /enterprises/:enterprise/actions/runners/:runner_id/labels",
-    ],
     listOrgAccessToSelfHostedRunnerGroupInEnterprise: [
       "GET /enterprises/:enterprise/actions/runner-groups/:runner_group_id/organizations",
     ],
     listRunnerApplicationsForEnterprise: [
       "GET /enterprises/:enterprise/actions/runners/downloads",
-    ],
-    listSelectedOrganizationsEnabledGithubActionsEnterprise: [
-      "GET /enterprises/:enterprise/actions/permissions/organizations",
     ],
     listSelfHostedRunnerGroupsForEnterprise: [
       "GET /enterprises/:enterprise/actions/runner-groups",
@@ -157,32 +119,14 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
     listSelfHostedRunnersInGroupForEnterprise: [
       "GET /enterprises/:enterprise/actions/runner-groups/:runner_group_id/runners",
     ],
-    removeAllCustomLabelsFromSelfHostedRunnerForEnterprise: [
-      "DELETE /enterprises/:enterprise/actions/runners/:runner_id/labels",
-    ],
-    removeCustomLabelFromSelfHostedRunnerForEnterprise: [
-      "DELETE /enterprises/:enterprise/actions/runners/:runner_id/labels/:name",
-    ],
     removeOrgAccessToSelfHostedRunnerGroupInEnterprise: [
       "DELETE /enterprises/:enterprise/actions/runner-groups/:runner_group_id/organizations/:org_id",
     ],
     removeSelfHostedRunnerFromGroupForEnterprise: [
       "DELETE /enterprises/:enterprise/actions/runner-groups/:runner_group_id/runners/:runner_id",
     ],
-    setAllowedActionsEnterprise: [
-      "PUT /enterprises/:enterprise/actions/permissions/selected-actions",
-    ],
-    setCustomLabelsForSelfHostedRunnerForEnterprise: [
-      "PUT /enterprises/:enterprise/actions/runners/:runner_id/labels",
-    ],
-    setGithubActionsPermissionsEnterprise: [
-      "PUT /enterprises/:enterprise/actions/permissions",
-    ],
     setOrgAccessToSelfHostedRunnerGroupInEnterprise: [
       "PUT /enterprises/:enterprise/actions/runner-groups/:runner_group_id/organizations",
-    ],
-    setSelectedOrganizationsEnabledGithubActionsEnterprise: [
-      "PUT /enterprises/:enterprise/actions/permissions/organizations",
     ],
     setSelfHostedRunnersInGroupForEnterprise: [
       "PUT /enterprises/:enterprise/actions/runner-groups/:runner_group_id/runners",
@@ -191,69 +135,19 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
       "PATCH /enterprises/:enterprise/actions/runner-groups/:runner_group_id",
     ],
   },
+  oidc: {
+    getOidcCustomSubTemplateForOrg: [
+      "GET /orgs/:org/actions/oidc/customization/sub",
+    ],
+    updateOidcCustomSubTemplateForOrg: [
+      "PUT /orgs/:org/actions/oidc/customization/sub",
+    ],
+  },
   orgs: {
-    createCustomRepoRole: ["POST /orgs/:org/custom-repository-roles"],
-    createCustomRole: [
-      "POST /orgs/:org/custom_roles",
-      {},
-      {
-        deprecated:
-          "octokit.scim.createCustomRole() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/reference/orgs#create-a-custom-role",
-      },
-    ],
-    deleteCustomRepoRole: [
-      "DELETE /orgs/:org/custom-repository-roles/:role_id",
-    ],
-    deleteCustomRole: [
-      "DELETE /orgs/:org/custom_roles/:role_id",
-      {},
-      {
-        deprecated:
-          "octokit.scim.deleteCustomRole() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/reference/orgs#delete-a-custom-role",
-      },
-    ],
     getAuditLog: ["GET /orgs/:org/audit-log"],
-    getCustomRepoRole: ["GET /orgs/:org/custom-repository-roles/:role_id"],
-    getCustomRole: [
-      "GET /orgs/:org/custom_roles/:role_id",
-      {},
-      {
-        deprecated:
-          "octokit.scim.getCustomRole() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/reference/orgs/#get-a-custom-role",
-      },
-    ],
-    listCustomRepoRoles: ["GET /orgs/:org/custom-repository-roles"],
-    listCustomRoles: [
-      "GET /organizations/:organization_id/custom_roles",
-      {},
-      {
-        deprecated:
-          "octokit.scim.listCustomRoles() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/reference/orgs#deprecated-list-custom-repository-roles-in-an-organization",
-      },
-    ],
-    listFineGrainedPermissions: [
-      "GET /orgs/:org/fine_grained_permissions",
-      {},
-      {
-        deprecated:
-          "octokit.scim.listFineGrainedPermissions() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/reference/orgs#list-repository-fine-grained-permissions-for-an-organization",
-      },
-    ],
-    listRepoFineGrainedPermissions: [
-      "GET /orgs/:org/repository-fine-grained-permissions",
-    ],
     listSamlSsoAuthorizations: ["GET /orgs/:org/credential-authorizations"],
     removeSamlSsoAuthorization: [
       "DELETE /orgs/:org/credential-authorizations/:credential_id",
-    ],
-    updateCustomRepoRole: ["PATCH /orgs/:org/custom-repository-roles/:role_id"],
-    updateCustomRole: [
-      "PATCH /orgs/:org/custom_roles/:role_id",
-      {},
-      {
-        deprecated:
-          "octokit.scim.updateCustomRole() is deprecated, see https://docs.github.com/enterprise-cloud@latest//rest/reference/orgs#update-a-custom-role",
-      },
     ],
   },
   scim: {
@@ -270,17 +164,6 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
     ],
     updateAttributeForUser: [
       "PATCH /scim/v2/organizations/:org/Users/:scim_user_id",
-    ],
-  },
-  secretScanning: {
-    getSecurityAnalysisSettingsForEnterprise: [
-      "GET /enterprises/:enterprise/code_security_and_analysis",
-    ],
-    patchSecurityAnalysisSettingsForEnterprise: [
-      "PATCH /enterprises/:enterprise/code_security_and_analysis",
-    ],
-    postSecurityProductEnablementForEnterprise: [
-      "POST /enterprises/:enterprise/:security_product/:enablement",
     ],
   },
   teams: {
