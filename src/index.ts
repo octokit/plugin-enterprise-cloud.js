636789c2a54bd4fcecdf494d46ee4c6125229e81
wolfy1339 <webmaster@wolfy1339.com>
2023-12-01 18:18:08 -0500

    refactor: use full relative path imports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import type { Octokit } from "@octokit/core";
 
-import ENDPOINTS from "./generated/endpoints";
-import { VERSION } from "./version";
-import { endpointsToMethods } from "./endpoints-to-methods";
+import ENDPOINTS from "./generated/endpoints.js";
+import { VERSION } from "./version.js";
+import { endpointsToMethods } from "./endpoints-to-methods.js";
 
 export function enterpriseCloud(octokit: Octokit) {
   return endpointsToMethods(octokit, ENDPOINTS);
