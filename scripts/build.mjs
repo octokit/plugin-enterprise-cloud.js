45e53da195fa1afd5342bdb41c42397d7b3adda4
wolfy1339 <webmaster@wolfy1339.com>
2024-05-09 12:58:09 -0400

    style: prettier

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -67,7 +67,7 @@ async function main() {
             import: "./dist-bundle/index.js",
             // Tooling currently are having issues with the "exports" field when there is no "default", ex: TypeScript, eslint
             default: "./dist-bundle/index.js",
-          }
+          },
         },
         sideEffects: false,
       },
